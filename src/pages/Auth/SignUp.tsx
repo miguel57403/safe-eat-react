446b5c75053aa287ae6bc43ecd49115decc18f3f
daniel57060 <a57060@alunos.ipb.pt>
2023-07-08 15:55:18 +0100

    Update LoginPage(), SignUpPage()

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,12 +1,16 @@
 import { FontsDefault } from "assets/fonts/Fonts";
-import { ContentSignUp } from "components/Contents/ContentSignUp";
-import { StyledLogin } from "pages/Auth/styled";
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import logo from "assets/images/logo-restaurants.svg";
+import { FormSignUp } from "components/Forms/SignUp";
 
 export const SignUpPage = () => {
   return (
-    <StyledLogin>
-      <ContentSignUp />
+    <Page>
+      <img src={logo} />
+      <Form>
+        <FormSignUp className="form-content" />
+      </Form>
       <div className="footer-content">
         <FontsDefault.P1 color="white">
           Already have an account?{" "}
@@ -15,6 +19,41 @@ export const SignUpPage = () => {
           </Link>
         </FontsDefault.P1>
       </div>
-    </StyledLogin>
+    </Page>
   );
 };
+
+export const Page = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  background-color: #22075e;
+
+  & .link-form {
+    margin-left: 5px;
+  }
+  & .link-form:visited {
+    color: lightgray;
+  }
+
+  & > img {
+    width: 200px;
+  }
+`;
+
+export const Form = styled.div`
+  width: 30%;
+  min-width: 300px;
+  background-color: white;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 0 4px 1px white;
+  height: max-content;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
