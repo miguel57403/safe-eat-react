1c7a3b9ae1fb8c7b00fa4de447f5c25c7a5aa861
daniel57060 <a57060@alunos.ipb.pt>
2023-07-08 22:07:45 +0100

    Update dataMocketTable.tsx

--- src/assets/data/dataMocketTable.tsx
+++ src/assets/data/dataMocketTable.tsx
@@ -1,78 +1,89 @@
+import { Tag } from "antd";
 import type { ColumnsType } from "antd/es/table";
 
 interface IDataType {
   key: string;
-  name: string;
-  category: string;
-  price: string;
+  client: string;
+  status: string;
+  quantity: string;
+  total: string;
 }
+
+const statusMap: Record<string, string> = {
+  REGISTERED: "volcano",
+  PREPARING: "yellow",
+  TRANSPORTING: "geekblue",
+  DELIVERED: "green",
+  CANCELED: "red",
+};
 
 export const ColumnsMockedProducts: ColumnsType<IDataType> = [
   {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
+    title: "Client",
+    dataIndex: "client",
+    key: "client",
     className: "column-table",
-    render: (text) => <a>{text}</a>,
   },
   {
-    title: "Category",
-    dataIndex: "category",
-    key: "category",
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
     className: "column-table",
-    render: (text) => <a>{text}</a>,
+    render: (_, { status }) => (
+      <Tag color={statusMap[status]} key={status}>
+        {status.toUpperCase()}
+      </Tag>
+    ),
   },
   {
-    title: "Price",
-    dataIndex: "price",
+    title: "Quantity",
+    dataIndex: "quantity",
     className: "column-table",
-    key: "price",
-    render: (text) => <a>{text}</a>,
+    key: "quantity",
+  },
+  {
+    title: "Total",
+    dataIndex: "total",
+    className: "column-table",
+    key: "total",
   },
 ];
 
 export const DataMockedProducts: IDataType[] = [
   {
     key: "1",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
+    client: "Daniel",
+    status: "REGISTERED",
+    total: "8,00",
+    quantity: "1",
   },
   {
-    key: "2",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
+    key: "1",
+    client: "Gustavo",
+    status: "PREPARING",
+    total: "8,00",
+    quantity: "1",
   },
   {
-    key: "3",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
+    key: "1",
+    client: "Miguel",
+    status: "TRANSPORTING",
+    total: "8,00",
+    quantity: "1",
   },
   {
-    key: "4",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
+    key: "1",
+    client: "Luiz",
+    status: "DELIVERED",
+    total: "8,00",
+    quantity: "1",
   },
   {
-    key: "5",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
-  },
-  {
-    key: "6",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
-  },
-  {
-    key: "7",
-    name: "Francesinha",
-    price: "8,00",
-    category: "Sanduish",
+    key: "1",
+    client: "Alan",
+    status: "CANCELED",
+    total: "8,00",
+    quantity: "1",
   },
 ];
 
@@ -84,8 +95,5 @@ export const rowSelection = {
       selectedRows
     );
   },
-  getCheckboxProps: (record: IDataType) => ({
-    disabled: record.name === "Disabled User", // Column configuration not to be checked
-    name: record.name,
-  }),
+  getCheckboxProps: (record: IDataType) => ({}),
 };
